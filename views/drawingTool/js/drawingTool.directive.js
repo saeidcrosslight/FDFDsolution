bfb843bec4126d1b70b47f781e8dc8b89854440a
Saeid Sadeh <33357113+saeidcrosslight@users.noreply.github.com>
2017-11-28 17:19:06 -0800

    fixup

--- views/drawingTool/js/drawingTool.directive.js
+++ views/drawingTool/js/drawingTool.directive.js
@@ -7,17 +7,29 @@ angular.module('drawingTool.directive', [])
     .controller('DrawingToolController', ['$scope', function($scope) {
         $scope.master = {};
 
-        $scope.update = function(circle) {
+        $scope.drawCircle = function(circle) {
             $scope.master = angular.copy(circle);
             var circ = new fabric.Circle({
-                left: 50,
-                top: 20,
+                left: 100,
+                top: 50,
                 fill: circle.color,
                 radius: circle.radius,
             });
             canvasWindow.add(circ);
+            circ.on('selected', function () {
+                console.log('selected!')
+            })
+            circ.on('moving', function () {
+                console.log('moving!')
+            })
+            var radiusX= circ.getRadiusX();
+            var radiusY = circ.getRadiusY();
+            var cord = circ.getCoords();
+            console.log(radiusX);
+            console.log(radiusY);
+            console.log(cord);
         };
-        $scope.update1 = function(rectangular) {
+        $scope.drawRectangular = function(rectangular) {
             $scope.master = angular.copy(rectangular);
             var rect = new fabric.Rect({
                 left: 100,
@@ -28,10 +40,17 @@ angular.module('drawingTool.directive', [])
                 angle: rectangular.angle
             });
             canvasWindow.add(rect);
+            rect.on('selected', function () {
+                console.log('selected!')
+            })
+            rect.on('moving', function () {
+                console.log('moving!')
+            })
         };
 
         $scope.reset = function() {
             $scope.circle = angular.copy($scope.master);
+            $scope.rectangular = angular.copy($scope.master);
         };
 
 
